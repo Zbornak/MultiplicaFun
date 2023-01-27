e95332356e28d52a8c740cb969cb0f8b7df7f114
Mark <zbornack@protonmail.com>
2023-01-27 13:03:43 +0000

    Basic styling

--- MultiplicaFun/ContentView.swift
+++ MultiplicaFun/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var multiplicand = 0
+    @State private var multiplier = 0
+    @State private var userPracticeChoice = 2
+    @State private var userAnswer = 0
+    @State private var userName = ""
+    @State private var questionAnimationAmount = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("What is your name?")
+                    TextField("enter name", text: $userName)
+                        .textFieldStyle(.roundedBorder)
+                }
+                .padding()
+                HStack {
+                    Text("🐮")
+                        .font(.system(size: 100))
+                    Text("Hi \(userName)! I'm your cowculator and I'm going to test your skills!")
+                        .fontWeight(.bold)
+                }
+                
+                HStack {
+                    Stepper("What do you want to practice?", value: $userPracticeChoice, in: 2...12)
+                    Text("\(userPracticeChoice)")
+                }
+                .padding()
+                
+                VStack {
+                    Text("What is...")
+                    Text("\(multiplicand) ✖️ \(multiplier) ⁉️")
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                        .scaleEffect(questionAnimationAmount)
+                        .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true), value: questionAnimationAmount)
+                        .onAppear {
+                            questionAnimationAmount = 1.0
+                        }
+                    HStack {
+                        Text("Your answer:")
+                        TextField("enter answer", value: $userAnswer, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                        Button("Go!") {
+                            
+                        }
+                        .padding()
+                    }
+                }
+                .padding()
+                .border(.black, width: 3)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding()
+            }
+            .navigationTitle("MultiplicaFun!")
         }
-        .padding()
     }
 }
 
