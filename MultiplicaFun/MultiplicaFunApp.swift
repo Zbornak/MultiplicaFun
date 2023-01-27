d1751116dd09ca82a426dae7a786c931a0497f56
Mark <zbornack@protonmail.com>
2023-01-27 10:56:51 +0000

    Initial Commit

---
+++ MultiplicaFun/MultiplicaFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicaFunApp.swift
+//  MultiplicaFun
+//
+//  Created by Mark Strijdom on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicaFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
